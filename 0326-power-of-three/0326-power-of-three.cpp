8b2235162b1bf541880932839b3c5854c8e56bae
Dhruv Jain <dhruvjainofficials@gmail.com>
2026-07-07 17:36:00 +0530

    Time: 4 ms (45.33%), Space: 8.9 MB (74.54%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n==1){
-            return true;
-        }
         if (n==-1 or n==0){
             return false;
         }
         else{
             while(n%3==0){
-                    n=n/3;
-                    cout<<"n="<<n<<endl;
-            }
-            if(n==1){
-                return true;
-            }
-            else{
-                return false;
+                n=n/3;
             }
         }
+        return n==1;
     }
 };
