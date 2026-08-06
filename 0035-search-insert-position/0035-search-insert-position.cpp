371015e23488af42c1ce0df241c47b31ab01c4c3
Dhruv Jain <dhruvjainofficials@gmail.com>
2026-08-07 00:00:22 +0530

    Time: 0 ms (100%), Space: 13.7 MB (44.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,19 +7,19 @@ public:
         if(target>nums[high-1]){
             return high;
         }
+
         while(low<=high){
-              mid=(low+high)/2;
-            if(nums[mid]==target){  
+            mid=(low+high)/2;
+            if(nums[mid]==target){
                 return mid;
             }
-          
-            if(target<nums[mid]){     
-            high=mid-1;    
-            }else{
-            low=mid+1;        
+            else if(nums[mid]>target){
+                high=mid-1;
             }
-          
+            else{
+                low=mid+1;
+            }
         }
-         return  low;   
+        return low;
     }
 };
